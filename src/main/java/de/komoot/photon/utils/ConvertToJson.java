a201f31454053b5eae90dd1dc4580410f607343f
Hendrik Morée <hendrik@flitsmeister.nl>
2019-05-22 07:59:53 +0200

    Strings has "

--- src/main/java/de/komoot/photon/utils/ConvertToJson.java
+++ src/main/java/de/komoot/photon/utils/ConvertToJson.java
@@ -50,7 +50,7 @@ public class ConvertToJson implements OneWayConverter<SearchResponse, List<JSONO
             for (String key : KEYS_LANG_SPEC) {
                 if (source.containsKey(key)) {
                     String localised = getLocalised(source, key, lang);
-                    String refValue = getLocalised(source, key, 'ref');
+                    String refValue = getLocalised(source, key, "ref");
                     properties.put(key, localised);
                     if (refValue != null && (localised == null || !localised.equals(refValue)))
                         properties.put(key + ":ref", refValue);
